cdb3c7618455f06450e5a56c87d3e3f8151a6a41
Berhanu27 <berhanumulu2022@gmail.com>
2026-02-25 11:13:53 +0300

    Delete stop-all.bat

--- stop-all.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Stopping all EventHub services...
-echo.
-echo Stopping Docker containers...
-docker-compose down
-echo.
-echo Done! All services stopped.
-echo Note: Stop backend and frontend manually with Ctrl+C in their terminals
-pause
